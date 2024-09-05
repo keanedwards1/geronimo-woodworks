c1072f94a940524910736dfb75448f7b847073d8
keanedwards1 <112280196+keanedwards1@users.noreply.github.com>
2024-09-05 16:54:49 -0700

    update

--- js/copy-email.js
+++ js/copy-email.js
@@ -6,12 +6,12 @@ document.getElementById('contactButton').addEventListener('click', function(e) {
         // Change tooltip text to indicate the email was copied
         const tooltip = document.getElementById('tooltip');
         tooltip.textContent = 'Email Copied!';
-        tooltip.style.marginLeft = 10 + 'px';
+        tooltip.style.marginLeft = 0 + 'px';
         
         // Reset the tooltip text after 2 seconds
         setTimeout(() => {
             tooltip.textContent = 'Copy Email';
-            tooltip.style.marginLeft = 18 + 'px';
+            tooltip.style.marginLeft = 0 + 'px';
         }, 2000);
     }).catch(err => {
         console.error('Failed to copy email:', err);
